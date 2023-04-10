0da8b005da45291ddb2d011d660db490677fc413
aster <more.alexandre11@gmail.com>
2023-04-10 04:19:52 -0300

    Curso and Mentoria ADD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Aprenda tudo sobre Java");
+        curso.setCargaHoraria(90);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso C#");
+        curso2.setDescricao("Aprenda tudo sobre C#");
+        curso2.setCargaHoraria(60);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Ajudando a entender sobre Java");
+        mentoria.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria de C#");
+        mentoria2.setDescricao("Ajudando a entender sobre C#");
+        mentoria2.setData(LocalDate.of(2023, 4, 15));
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+        System.out.println(mentoria2);
     }
 }
