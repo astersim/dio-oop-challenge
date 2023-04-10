a32bc8cd97e2c9f82f86f4e4e0725288f51a5e79
aster <more.alexandre11@gmail.com>
2023-04-10 13:42:24 -0300

    Final project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -26,9 +25,45 @@ public class Main {
         mentoria2.setDescricao("Ajudando a entender sobre C#");
         mentoria2.setData(LocalDate.of(2023, 4, 15));
 
-        System.out.println(curso);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-        System.out.println(mentoria2);
+        Curso cursoOnline = new Curso();
+        cursoOnline.setTitulo("Novo curso online sobre Python");
+        cursoOnline.setDescricao("Aprenda tudo sobre Python");
+        cursoOnline.setCargaHoraria(60);
+
+
+//        System.out.println(curso);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+//        System.out.println(mentoria2);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCamp Varias Linguagens");
+        bootcamp.setDescricao("Diversos conhecimentos sobre diversas linguagens");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(cursoOnline);
+
+        Dev devAlexandre = new Dev();
+        devAlexandre.setNome("Alexandre");
+        devAlexandre.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Alexandre: " + devAlexandre.getConteudosInscritos());
+        devAlexandre.progredir();
+        devAlexandre.progredir();
+        devAlexandre.progredir();
+        System.out.println("*____*");
+        System.out.println("Conteudos Inscritos Alexandre: " + devAlexandre.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Alexandre: " + devAlexandre.getConteudosConcluidos());
+        System.out.println("XP:" + devAlexandre.calcularXP());
+
+        Dev devLiz = new Dev();
+        devLiz.setNome("Liz");
+        devLiz.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Liz: " + devLiz.getConteudosInscritos());
+        devLiz.progredir();
+        devLiz.progredir();
+        System.out.println("*____*");
+        System.out.println("Conteudos Inscritos Liz: " + devLiz.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Liz: " + devLiz.getConteudosConcluidos());
+        System.out.println("XP:" + devLiz.calcularXP());
     }
 }
